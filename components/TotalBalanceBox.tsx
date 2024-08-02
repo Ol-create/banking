0ef539ec77f58d03e71dc563a4989206dd5e7afd
Ol-create <oluola96@gmail.com>
2024-08-02 18:21:48 +0100

    add DoughnutChat Component

--- components/TotalBalanceBox.tsx
+++ components/TotalBalanceBox.tsx
@@ -2,14 +2,15 @@ import { formatAmount } from '@/lib/utils'
 import React from 'react'
 import CountUp from 'react-countup'
 import AnimatedCounter from '@/components/AnimatedCounter'
+import DoughnutChart from './DoughnutChart'
 
 const TotalBalanceBox = ({
-    account=[], totalBanks, totalCurrentBalance 
+    accounts=[], totalBanks, totalCurrentBalance 
 }: TotlaBalanceBoxProps) => {
   return (
     <section className='total-balance'>
     <div className='total-balance-chart'>
-        {/* Doghnut Chart */}
+        <DoughnutChart accounts={accounts} />
     </div>
     <div className='flex flex-col gap-6'>
       <h2 className='header-2'>
