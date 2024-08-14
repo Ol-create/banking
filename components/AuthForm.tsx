03879e105e361798d138a6dd647e76e3e46023a8
Ol-create <oluola96@gmail.com>
2024-08-14 23:00:09 +0100

    add appwrite session

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -20,8 +20,11 @@ import { Input } from "@/components/ui/input"
 import { Divide, Loader2 } from 'lucide-react'
 import CustomInput from './CustomInput'
 import { authFormSchema } from '@/lib/utils'
+import { useRouter } from 'next/navigation'
+import { signIn, signUp } from '@/lib/actions/user.actions'
 
 const AuthForm = ({ type }: { type: string }) => {
+  const router = useRouter()
     const [user, setUser] = useState(null)
     const [isLoading, setIsLoading] = useState(false)
 
@@ -34,12 +37,30 @@ const AuthForm = ({ type }: { type: string }) => {
       password: "",
     },
   }) 
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  const onSubmit = async (data: z.infer<typeof formSchema>) {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
     setIsLoading(true)
-    console.log(values)
+    try {
+      // Sign up with Appwrite & create plaid token
+      if (type === 'sign-up') {
+         const newUser = await signUP(data)
+        setUser(newUser);
+      }
+      if(type === 'sign-in') {
+         const response = await signIn({
+          email: data.email,
+          password: data.password,
+         })
+         if (response) router.push('/')
+      }
+     
+
+    } catch (error) {
+      console.log(error)
+    } finally {
     setIsLoading(false)
+    }
   }
   return (
     <section className='auth-form'>
---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,17 @@
+'use-server';
+
+export const signIn = async () => {
+    try {
+        // Mutation / Database / Make fetch
+    } catch (error) {
+        console.log('Error ', error);
+    }
+}
+
+export const signUp = async ( user: SignUpParams) => {
+    try {
+        // Create a user acount
+    } catch (error) {
+        console.log('Error ', error);
+    }
+}
